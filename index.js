53bce1d3b696b4b78d8acfa6e350751923716a89
tareq <tareq.al-abbas@live.com>
2018-02-18 00:40:42 +0200

    removed unneeded cross origin policy

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const path = require('path');
 const offers = require('./src/responseHandlers/offers');
-
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 //serves the static homepage
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/', 'index.html')));
